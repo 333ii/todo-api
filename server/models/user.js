71d432e9e15e86fd2866ee7450e0f4d904665060
eicdafusen <bahadirhamzagul@gmail.com>
2018-01-31 16:50:51 +0300

    add auth

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,89 @@
-var mongoose  = require('mongoose');
+const mongoose  = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
 
-var User = mongoose.model('User',{
+var UserSchema = new mongoose.Schema({
 	email: {
 		type:String,
 		required: true,
-		trim:true,
-		minlength:1
-	}
+		trim: true,
+		minlength: 1,
+		unique: true,
+		validate: {
+			validator: validator.isEmail,
+			message: '{VALUE} is not a valid adress'
+		}
+	},
+
+	password:{
+		type:String,
+		required:true,
+		minlength:6
+	},
+
+	tokens:[{
+
+		access:{
+			type:String,
+			required:true
+		},
+		token:{
+			type:String,
+			required:true
+		}
+	}]	
 });
+
+
+UserSchema.statics.findByToken=function(token){
+
+	var User = this;
+	var decoded;
+
+	 
+	try{
+
+	decoded = jwt.verify(token,'abc123');
+
+	}catch(e){
+
+		return Promise.reject();
+	}
+
+	return User.findOne({
+		'_id':decoded._id,
+		'tokens.token':token,
+		'tokens.access':'auth'
+	});
+}
+
+UserSchema.methods.toJSON=function(){
+
+	var user = this;
+	
+	return {
+		_id:user._id,
+		email:user.email
+	};
+
+}
+
+UserSchema.methods.generateAuthToken = function(){
+	var user  = this;
+	var access	 = 'auth';
+
+	var token = jwt.sign({_id: user._id.toHexString(),access},'abc123').toString(); 
+
+	user.tokens.push({access,token});
+
+	return user.save().then(()=>{
+		return token;
+	});
+
+};
+
+
+var User = mongoose.model('User',UserSchema);
 
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const _ =require('lodash');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT ||  3000;
@@ -64,6 +66,64 @@ app.delete('/todos/:id',(req,res)=>{
 
 
 });
+
+app.patch('/todos/:id',(req,res)=>{
+
+	var id = req.params.id;
+	var body = _.pick(req.body , ['text','completed']);
+
+	if(_.isBoolean(body.completed) && body.completed){
+
+		body.completedAt= new Date().getTime();			
+
+	}else{
+		body.completed = false;
+		body.completedAt = null;	
+	}
+
+	Todo.findByIdAndUpdate(id,{$set: body},{new: true}).then((todo)=>{
+
+			if(!todo){
+				res.status(404).send();
+			}
+
+			res.send({todo});
+
+	}).catch((e)=>{
+		res.status(400).send(e);
+	});	
+	
+
+});
+
+
+app.post('/users',(req,res)=>{
+
+	var user = new User({
+		email:req.body.email,	
+		password:req.body.password
+	})
+
+	user.save().then(()=>{
+		return user.generateAuthToken();
+	}).then((token)=>{
+		res.header('x-auth',token).send(user);
+	}).catch((err)=>{
+			res.status(400).send(err);
+	});
+
+})
+
+
+
+
+
+app.get('/users/me',authenticate,(req,res)=>{
+
+	res.send(req.user);
+});
+
+
 
 
 app.listen(port,()=>{
