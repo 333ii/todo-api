639b67f79902903354adea61ea3c57429bcaff5e
eicdafusen <bahadirhamzagul@gmail.com>
2018-02-01 16:55:01 +0300

    pass hash login

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,7 @@
 const mongoose  = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
 	email: {
@@ -82,6 +83,56 @@ UserSchema.methods.generateAuthToken = function(){
 
 };
 
+UserSchema.pre('save',function(next){
+
+	var user = this;
+
+	if(user.isModified('password')){
+
+		bcrypt.genSalt(10,(err,salt)=>{
+
+			bcrypt.hash(user.password,salt,(err,hash)=>{
+				user.password = hash;
+				next();
+			});
+		});
+	}else{
+		next();
+	}
+});
+
+UserSchema.statics.findByCredentials= function (email,password){
+
+	var user = this;
+
+	return User.findOne({email}).then((user)=>{
+
+		if(!user){
+			return Promise.reject();
+		};
+
+
+
+		return new Promise((resolve,reject)=>{
+
+			bcrypt.compare(password,user.password,(err,res)=>{
+
+			if(res){
+
+				return resolve(user);
+			}else{
+
+				return reject();
+			}
+
+			});
+
+		});
+		
+
+	});
+	
+};
 
 var User = mongoose.model('User',UserSchema);
 
--- server/server.js
+++ server/server.js
@@ -123,7 +123,17 @@ app.get('/users/me',authenticate,(req,res)=>{
 	res.send(req.user);
 });
 
+app.post('/users/login',(req,res)=>{
 
+	User.findByCredentials(req.body.email,req.body.password).then((user)=>{
+
+		user.generateAuthToken().then((token)=>{
+			res.header('x-auth',token).send(user);
+		});
+	}).catch((err)=>{
+			res.status(400).send();
+	});
+});
 
 
 app.listen(port,()=>{
